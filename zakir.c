c7ac0b178152deefda01f59d4541154cdd5af111
mdzakir1997 <35652332+mdzakir1997@users.noreply.github.com>
2018-01-21 14:57:41 +0530

    Create zakir.c

---
+++ zakir.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a;
+printf("Enter an number");
+scanf("\n%d",&a);
+if(a>0)
+{
+printf("\npositive");
+}
+else if(a<0)
+{
+printf("\nnegative");
+}
+else
+{
+printf("\nZero");
+}
+getch();
+}
