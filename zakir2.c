52f340def366c2311fc8aded146f00e8bc7ec103
mdzakir1997 <35652332+mdzakir1997@users.noreply.github.com>
2018-01-21 15:13:38 +0530

    Create zakir2.c

---
+++ zakir2.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+int a;
+printf("\nEnter a number");
+scanf("%d",&a);
+if(a%2==0)
+{
+printf("\nThe number is even");
+}
+else
+{
+printf("\nThe number is odd");
+}
+}
