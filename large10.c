958ce94db808206da34a6bd7902c2ccff09a99e9
mdzakir1997 <35652332+mdzakir1997@users.noreply.github.com>
2018-03-01 16:32:34 +0530

    Rename large.c to large10.c

---
+++ large10.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+void main()
+{
+    int a[10],i,n,j;
+    printf("\nEnter the size of the array");
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        for(j=i+1;j<n;j++)
+        {
+            if(a[i]>a[j])
+            {
+             int t;
+             t=a[i];
+             a[i]=a[j];
+             a[j]=t;
+            }
+        }
+    }
+    printf("\nThe largest number is %d",a[n-1]);
+}
